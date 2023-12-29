3b8c02644a15d2d5b2745cb0cdaa3eab69995909
kumaryoya <kumaryoya05210521@gmail.com>
2023-12-29 18:06:49 +0900

    エラー修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://movie-app-front-9iph.vercel.app/'
+    origins 'localhost:3000', 'https://movie-app-front-9iph.vercel.app'
     resource '*',
       headers: :any,
       methods: %i[get post put patch delete options head]
